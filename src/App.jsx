c45b7d73d5f2df2b88cdba1d4a6b0c2a2b40ed6e
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-02 18:10:29 -0400

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import AddProduct from "./pages/AddProduct";
+import ListProducts from "./pages/ListProducts";
+import Orders from "./pages/Orders";
 const App = () => {
 	return (
 		<div className="bg-gray-50 min-h-screen">
@@ -10,6 +14,19 @@ const App = () => {
 				<hr />
 				<div className="flex w-full">
 					<Sidebar />
+					<div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+						<Routes>
+							<Route
+								path="/add-product"
+								element={<AddProduct />}
+							></Route>
+							<Route
+								path="/list"
+								element={<ListProducts />}
+							></Route>
+							<Route path="/orders" element={<Orders />}></Route>
+						</Routes>
+					</div>
 				</div>
 			</>
 		</div>
