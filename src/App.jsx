3ed9dffd961e0d5ae1ea4b7fa4e7ecf9bd621f21
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-02 18:31:24 -0400

    create login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import AddProduct from "./pages/AddProduct";
 import ListProducts from "./pages/ListProducts";
 import Orders from "./pages/Orders";
+import Login from "./components/Login";
 const App = () => {
+	const [token, setToken] = useState("");
 	return (
 		<div className="bg-gray-50 min-h-screen">
-			<>
-				<Navbar />
+			{token === "" ? (
+				<Login />
+			) : (
+				<>
+					<Navbar />
 
-				<hr />
-				<div className="flex w-full">
-					<Sidebar />
-					<div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-						<Routes>
-							<Route
-								path="/add-product"
-								element={<AddProduct />}
-							></Route>
-							<Route
-								path="/list"
-								element={<ListProducts />}
-							></Route>
-							<Route path="/orders" element={<Orders />}></Route>
-						</Routes>
+					<hr />
+					<div className="flex w-full">
+						<Sidebar />
+						<div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+							<Routes>
+								<Route
+									path="/add-product"
+									element={<AddProduct />}
+								></Route>
+								<Route
+									path="/list"
+									element={<ListProducts />}
+								></Route>
+								<Route
+									path="/orders"
+									element={<Orders />}
+								></Route>
+							</Routes>
+						</div>
 					</div>
-				</div>
-			</>
+				</>
+			)}
 		</div>
 	);
 };
