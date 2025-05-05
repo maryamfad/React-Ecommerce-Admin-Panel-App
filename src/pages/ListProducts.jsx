8ff43377f9b6bc96b7a0fe51030d2a7b8063f318
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-05 14:17:58 -0400

    get list of all products from an api

--- src/pages/ListProducts.jsx
+++ src/pages/ListProducts.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../constants";
+import { toast } from "react-toastify";
 
 const ListProducts = () => {
+	const [list, setList] = useState([]);
+
+	const fetchList = async () => {
+		try {
+			const response = await axios.get(backendUrl + "/api/product/list");
+			if (response.success) {
+				setList(response.data.products);
+			} else {
+				toast.error(response.data.message);
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
+	};
+
+	useEffect(() => {
+		fetchList();
+	}, []);
 	return <div></div>;
 };
 
